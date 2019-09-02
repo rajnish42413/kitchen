bdb96e836fbfb7f8050195a62debb6265a8788ef
rajnish42413 <rajnish42413@gmail.com>
2019-09-02 23:50:13 +0530

    kit setup to git

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong>Copyright &copy; 2018-<?php echo date('Y') ?>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
